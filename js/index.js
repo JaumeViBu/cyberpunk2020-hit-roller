9b664f05628601cd64fa293f9bebe4b7b75c1edc
jaume vidal <jaumevibu@gmail.com>
2023-08-23 19:16:18 +0200

    added feature to choose weapon type and show the range values accordingly added feature to add damage dice formula to show in the results text area the damage rolled

--- js/index.js
+++ js/index.js
@@ -1,17 +1,21 @@
-const DEBUGMODE = true;
+const DEBUGMODE = false;
 
 function hitRoll() {
 
   let modifier = Number.parseInt(document.querySelector('#inHitMod').value);
-  const dice = d10Roll();
+  let dice = d10Roll();
   modifier = isNaN(modifier) ? 0 : modifier;
+  const resultsTextArea = document.querySelector('#txtResults');
+  let total = dice + modifier;
 
-  const total = dice + modifier;
+  resultsTextArea.value = `..............................\n` + resultsTextArea.value;
+  resultsTextArea.value = `total: ${dice} + ${modifier} = ${total}\n` + resultsTextArea.value;
 
   resetChecks();
   updateDistanceChecks(total);
   resetHitLocation();
   hitLocation();
+  rollWeaponDamage();
 
   if (DEBUGMODE) {
 
@@ -54,26 +58,32 @@ function hitLocation() {
 
   const location = d10Roll();
   const container = document.querySelector('#rightContainer');
-
+  const resultsTextArea = document.querySelector('#txtResults');
 
 
   if (location == 1) {
     container.classList.replace('no-hit', 'head-hit');
+    resultsTextArea.value = `location: head\n` + resultsTextArea.value;
   }
   if (location >= 2 && location <= 4) {
     container.classList.replace('no-hit', 'torso-hit');
+    resultsTextArea.value = `location: torso\n` + resultsTextArea.value;
   }
   if (location == 5) {
     container.classList.replace('no-hit', 'left-arm-hit');
+    resultsTextArea.value = `location: left arm\n` + resultsTextArea.value;
   }
   if (location == 6) {
     container.classList.replace('no-hit', 'right-arm-hit');
+    resultsTextArea.value = `location: right arm\n` + resultsTextArea.value;
   }
   if (location >= 7 && location <= 8) {
     container.classList.replace('no-hit', 'left-leg-hit');
+    resultsTextArea.value = `location: left leg\n` + resultsTextArea.value;
   }
   if (location >= 9 && location <= 10) {
     container.classList.replace('no-hit', 'right-leg-hit');
+    resultsTextArea.value = `location: right leg\n` + resultsTextArea.value;
   }
 
 
@@ -109,6 +119,73 @@ function randomIntFromInterval(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function weaponChange() {
+
+
+  const weaponName = document.querySelector('#selWeaponType').value;
+  const checkContact = document.querySelector('#distanceCheck-contact');
+  const checkClose = document.querySelector('#distanceCheck-close');
+  const checkMid = document.querySelector('#distanceCheck-mid');
+  const checkFar = document.querySelector('#distanceCheck-far');
+  const checkExtraFar = document.querySelector('#distanceCheck-extrafar');
+
+  const weaponDistances = {
+    pistol: 50,
+    submachine: 150,
+    shotgun: 50,
+    rifle: 400,
+    heavy: null,
+    exotic: null,
+  };
+
+  checkContact.textContent = `Contact (<=1m)`;
+  if (weaponDistances[weaponName]) {
+
+    checkClose.textContent = `Close (${0.25 * weaponDistances[weaponName]}m)`;
+    checkMid.textContent = `Mid (${0.5 * weaponDistances[weaponName]}m)`;
+    checkFar.textContent = `Far (${weaponDistances[weaponName]}m)`;
+    checkExtraFar.textContent = `Extra Far (${2 * weaponDistances[weaponName]}m)`;
+  } else {
+
+    checkClose.textContent = `Close (1/4 weapon distance)`;
+    checkMid.textContent = `Mid (1/2 weapon distance)`;
+    checkFar.textContent = `Far (weapon distance)`;
+    checkExtraFar.textContent = `Extra Far (2x weapon distance)`;
+  }
+
+}
+
+function rollWeaponDamage() {
+
+  const weaponDamage = document.querySelector('#inDmg').value;
+  const resultsTextArea = document.querySelector('#txtResults');
+
+  const parsed = weaponDamage.match('^([0-9]+)[d]([0-9]+)([+-][0-9]+)?$');
+
+  if (parsed) {
+
+    let dmg = 0;
+    for (let i = 0; i < parsed[1]; i += 1)dmg += randomIntFromInterval(1, parsed[2]);
+    if (parsed[3]) {
+      dmg += Number.parseInt(parsed[3]);
+    }
+    resultsTextArea.value = `damage: ${dmg}\n` + resultsTextArea.value;
+  } else {
+
+    resultsTextArea.value = `damage: -invalid formula-\n` + resultsTextArea.value;
+  }
+
+  if (DEBUGMODE) {
+
+    console.log(parsed)
+  }
+}
+
+function clearResults() {
+
+  document.querySelector('#txtResults').value = '';
+}
+
 
 addEventListener("submit", (e) => {
 
@@ -116,3 +193,4 @@ addEventListener("submit", (e) => {
   hitRoll();
 })
 
+weaponChange();
